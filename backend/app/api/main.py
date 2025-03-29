504d1eaa88e5546a4d8f6235935dba155078c934
blueoc-ducphong <phong.nguyen@blueoc.tech>
2025-03-29 08:20:47 +0700

    fix item

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, sub_todo, todos, users, utils
+from app.api.routes import login, private, sub_todo, todos, users, utils
 from app.core.config import settings
 
 api_router = APIRouter()
--- backend/app/api/routes/users.py
+++ backend/app/api/routes/users.py
@@ -9,7 +9,6 @@ from app.api.deps import CurrentUser, SessionDep, get_current_active_superuser
 from app.core.config import settings
 from app.core.security import get_password_hash, verify_password
 from app.models import (
-    Item,
     Message,
     UpdatePassword,
     User,
